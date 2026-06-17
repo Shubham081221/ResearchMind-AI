283f6c37924b57082a81bfcd4f22ab3c0a6f0b20
Shubham Kumar <shubhamkumarbarnwal2001@gmail.com>
2026-06-17 16:15:48 +0530

    Backend is done, ready to  deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,13 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+    ],
+    credentials: true,
+}));
+
 app.use(express.json());
 
 app.use("/api/research", researchRoutes);
