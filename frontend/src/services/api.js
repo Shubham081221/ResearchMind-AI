3613f092e4b8c185cf56fe2a2ba6a4fbe9a1098e
Shubham Kumar <shubhamkumarbarnwal2001@gmail.com>
2026-06-17 16:50:52 +0530

    .env is done, ready to  deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://researchmind-ai-eg6j.onrender.com/api",
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 // const api = axios.create({
